60503fc969c4ad3e924be24c23f01c25558214dc
Narayan Khanal <narayankhanal435@gmail.com>
2024-05-03 21:35:56 -0400

    use .gitignore to ignore node modules and otherunnecessary files

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express"); // load express module
 const nedb = require("nedb-promises"); // load nedb module
 const bcrypt = require("bcrypt"); // load bcrypt module
 const crypto = require("crypto"); // load crypto module
+const cors = require("cors"); // load cors middleware
 
 const app = express(); // init app
 const db = nedb.create("users.jsonl"); // init db
 app.use(express.static("public")); // enable static routing to "./public" folder
 // automatically decode all requests from JSON and encode all responses into JSON
 app.use(express.json());
+app.use(cors());
 
 // create route to get all user records (GET /users)
 app.get("/users", (req, res) => {
@@ -81,54 +83,79 @@ app.post('/users', (req, res) => {
     res.send({ error: 'Missing fields.' })
   }
 })
+// post route to add tasks for each loggedIn user
+app.post("/users/:username/tasks", (req, res)=>{
+  const {username} = req.params;
+  const {txt, authToken} = req.body;
 
-app.patch('/users/:username/:authToken', (req, res) => {
-  const { username, authToken } = req.params
-
-  db.findOne({ _id: username })
-    .then((user) => {
-      if (user && user.authToken === authToken) {
-        db.updateOne({ _id: username }, { $set: req.body })
-          .then((numUpdated) => {
-            if (numUpdated === 0) {
-              res.send({ error: 'Something went wrong.' })
-            } else {
-              res.send({ ok: true })
-            }
-          })
-          .catch((error) => res.send({ error }))
-      } else {
-        res.send({ error: 'Invalid authentication token.' })
-      }
-    })
-    .catch((error) => res.send({ error }))
+  db.findOne({_id: username, authToken: authToken})
+  .then(user =>{
+    if (user){
+      const task = {id: new Date().getTime(), txt, completed: false};
+      user.tasks.push(task);
+      db.updateOne({_id: username}, {$set: {tasks: user.tasks}})
+      .then(()=>{
+        res.send({tasks: user.tasks})})
+      .catch((error) =>{
+        res.status(500).send({error: "erroror occurred while updating tasks!"});
+      })
+    } else{
+      res.status(401).send({error: "Authentication failed!"}); //lack valid authentication credentials
+    }
+  })
+  .catch(error => res.status(500).send({error})); //internal server error
 })
 
-app.delete('/users/:username/:authToken', (req, res) => {
-  const { username, authToken } = req.params
 
-  db.findOne({ _id: username })
-    .then((user) => {
-      if (user && user.authToken === authToken) {
-        db.deleteOne({ _id: username })
-          .then((numDeleted) => {
-            if (numDeleted === 0) {
-              res.send({ error: 'Something went wrong.' })
-            } else {
-              res.send({ ok: true })
-            }
-          })
-          .catch((error) => res.send({ error }))
-      } else {
-        res.send({ error: 'Invalid authentication token.' })
-      }
-    })
-    .catch((error) => res.send({ error }))
+// patch route to update tasks with each unique taskId
+app.patch("/users/:username/tasks/:taskId", (req, res)=>{
+  const {username, taskId} = req.params;
+  const {txt, completed, authToken} = req.body;
+
+  db.findOne({_id: username, authToken: authToken})
+  .then(user => {
+    if (user){
+      const taskIdx = user.tasks.findIndex(task => task.id == taskId);
+
+      if(taskIdx > -1){
+        user.tasks[taskIdx] = {...user.tasks[taskIdx], txt, completed};
+
+      db.updateOne({_id: username}, {$set: {tasks: user.tasks}}) 
+      .then(()=> res.send({task: user.tasks[taskIdx]}))
+      .catch(error => res.status(500).send({error: "error Occurred while updating task!"}));
+    } else{
+      res.status(404).send({error: "Task not found!"});
+    }
+  } else{
+    res.status(401).send({error: "Authentication failed!"})
+  }
+  })
+  .catch(error => res.status(500).send({error}));
+});
+
+// delete route for deleting task using each taskId
+app.delete("/users/:username/tasks/:taskId", (req, res) =>{
+  const {username, taskId} = req.params;
+  const {authToken} = req.body;
+
+  db.findOne({_id: username, authToken: authToken})
+  .then(user =>{
+    if (user){
+      const filteredTasks = user.tasks.filter(task => task.id != taskId);
+      db.updateOne({_id: username}, {$set: {tasks: filteredTasks}})
+      .then(()=> res.send({ok: true}))
+      .catch(error => res.status(500).send({error: "erroror occurred while deleting task!"}));
+    } else{
+      res.status(401).send({error: "Authentication failed!"});
+    }
+  })
+  .catch(error => res.status(500).send({error}));
 })
+
 
 // default route
 app.all('*', (req, res) => {
-  res.status(404).send('Invalid URL.')
+  res.status(404).send('Invalid URL.');
 })
 
 // start server
--- script.js
+++ script.js
@@ -11,43 +11,57 @@ const signUpLink = $(".signUpLink");
 const addTaskBtn = $(".addTask-btn");
 const signOutBtn = $(".signOut-btn");
 const addTaskContainer = $(".addTaskContainer");
-const crossMark = $(".crossMark");
+const editTaskContainer = $(".editTaskContainer");
+const createCrossMark = $("#createCrossMark");
+const editCrossMark = $("#editCrossMark");
 const createTaskBtn = $("#submitTaskBtn");
+const editTaskBtn = $("#submitEditTaskBtn");
 const tasksContainer = $(".tasks");
-const inputTask = $(".inputTask");
-const mainContainer = $(".mainContainer");
+const inputAddTask = $(".inputAddTask");
+const inputEditTask = $(".inputEditTask");
+const credentialsContainer = $(".credentialsContainer");
 const mainPage = $(".mainPage");
-const editIcon = $("#editIcon");
-const deleteIcon = $("#deleteIcon");
+
+//clear targetInput Values
+const clearInputVals = () => {
+  let targetInputElements = document.querySelectorAll(".targetInput");
+  for (let eachTargetElement of targetInputElements) {
+    eachTargetElement.value = "";
+  }
+};
+
+// show Error
+const showError = (err) => {
+  $("#error").innerText = err;
+};
 
 // logInScreen
 const openLogInScreen = () => {
   signUpPage.classList.add("hidden-class");
   logInPage.classList.remove("hidden-class");
+  clearInputVals();
+  showError("");
 };
 // signUpScreen
 const openSignUpScreen = () => {
+  //hide credentialsContainer screen, clear inputs, clear error
   logInPage.classList.add("hidden-class");
   signUpPage.classList.remove("hidden-class");
-};
-// mainPageScreen
-const openMainPageScreen = (doc) =>{
-  //hide other screens, clear inputs, clear error
-  logInPage.classList.add("hidden-class");
-  // signUpPage.classList.add("hidden-class");
+  clearInputVals();
   showError("");
+};
 
-  //show mainPageScreen
+// mainPageScreen
+const openMainPageScreen = () =>{
+  const user = JSON.parse(localStorage.getItem("user"));
+  if (user){
+     //display user's name
+     $("#name").innerText = `${user.name}!`;
+  }
+  credentialsContainer.classList.add("hidden-class");
   mainPage.classList.remove("hidden-class");
-
-  //display name
-  $("#name").innerText = doc.name;
-
-
-}
-// show Error
-const showError = (err) => {
-  $("#error").innerText = err;
+  loadTasks();
+  
 };
 
 // logIn link action
@@ -57,50 +71,43 @@ logInLink.addEventListener("click", openLogInScreen);
 signUpLink.addEventListener("click", openSignUpScreen);
 
 // logIn button action
-logInBtn.addEventListener("click", () => {
-    const username = $(".loginUsername").value;
-    const password = $(".loginPassword").value;
-      // mainContainer.classList.add("hidden-class"); //hide mainContainer
-      // mainPage.classList.remove("hidden-class"); //show mainPage
-
-      //clear input values
-      // $(".inputUsername").value = "";
-      // $(".inputPassword").value = "";
-      if (username && password){
-        // use POST /users/auth to authentication
-      fetch("http://localhost:3000/users/auth", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, password }),
+logInBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const username = $("#loginUsername").value;
+  const password = $("#loginPassword").value;
+  if (username && password) {
+    // use POST /users/auth to authentication
+    fetch("http://localhost:3000/users/auth", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((res) => res.json())
+      .then((doc) => {
+        if (doc.error) {
+          showError(doc.error);
+        } else {
+          localStorage.setItem("authToken", doc.authToken);
+          localStorage.setItem("isLogIn", true);
+          localStorage.setItem("user", JSON.stringify(doc));
+          openMainPageScreen(doc);
+        }
       })
-        .then((res) => res.json())
-        .then((doc) => {
-          if (doc.error) {
-            showError(doc.error);
-          } else {
-            localStorage.setItem("authToken", doc.authToken);
-            openMainPageScreen(doc);
-          }
-        })
-        .catch((err) => showError("ERROR: " + err));
-      }
-      
-    });
-
-
+      .catch((err) => showError("ERROR: " + err));
+  }
+});
 // signUp button action
-signUpBtn.addEventListener("click", () => {
+signUpBtn.addEventListener("click", (e) => {
+  e.preventDefault();
   // grab all user info from input fields, and POST it to /users
   var data = {
-    username: $(".signUpUsername").value,
-    password: $(".signUpPassword").value,
-    name: $(".signUpName").value,
-    email: $(".signUpEmail").value,
+    username: $("#signUpUsername").value,
+    password: $("#signUpPassword").value,
+    name: $("#signUpName").value,
+    email: $("#signUpEmail").value,
   };
-  
-  
-  //POST /users
 
+  //POST /users
   fetch("http://localhost:3000/users", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -112,6 +119,8 @@ signUpBtn.addEventListener("click", () => {
         showError(doc.error);
       } else {
         localStorage.setItem("authToken", doc.authToken);
+        localStorage.setItem("isSignUp", true);
+        localStorage.setItem("user", JSON.stringify(doc));
         openMainPageScreen(doc);
       }
     })
@@ -120,45 +129,251 @@ signUpBtn.addEventListener("click", () => {
 
 // signOut button action
 signOutBtn.addEventListener("click", () => {
+  //remove all necessary items from the local storage after signing out
+  localStorage.removeItem("authToken");
+  localStorage.removeItem("isLogIn");
+  localStorage.removeItem("isSignUp");
   mainPage.classList.add("hidden-class"); //hide mainPage
-  mainContainer.classList.remove("hidden-class"); //show mainContainer
+  credentialsContainer.classList.remove("hidden-class"); //show credentialsContainer
+  openLogInScreen();
 });
 // addTask button action
 addTaskBtn.addEventListener("click", () => {
-  addTaskContainer.classList.remove("hidden-class"); //display addTaskContainer
+  addTaskContainer.classList.toggle("hidden-class"); //display addTaskContainer
+  // console.log(addTaskContainer.classList)
+  // console.log("clicked");
 });
 
-//crossMark icon action
-crossMark.addEventListener("click", () => {
-  addTaskContainer.classList.add("hidden-class"); //hide addTaskContainer
+//createCrossMark icon action
+createCrossMark.addEventListener("click", () => {
+  console.log("clicked");
+  //hides the add and edit task containers
+  addTaskContainer.classList.add("hidden-class");
+  
 });
+
+//editCrossMark icon action
+editCrossMark.addEventListener("click", ()=>{
+  editTaskContainer.classList.add("hidden-class");
+})
 
 //createTask button action
-createTaskBtn.addEventListener("click", () => {
-  if (inputTask.value) {
-    let divContainer = document.createElement("div");
-    divContainer.classList.add(
-      "flex",
-      "flex-row",
-      "rounded-sm",
-      "shadow-md",
-      "bg-slate-50",
-      "p-3",
-      "sm:p-4",
-      "my-3",
-      "mx-auto",
-      "justify-between",
-      "items-center",
-      "max-w-[35rem]"
-    );
-    divContainer.innerHTML = `<p>${inputTask.value}<p>
-    <div class = "icons flex gap-4">
-    <i id = "editIcon" class="text-base sm:text-lg fa-solid fa-pen-to-square cursor-pointer hover:text-slate-600 active:text-slate-600"></i>
-    <i id= "deleteIcon" class="text-base sm:text-lg fa-solid fa-trash hover:cursor-pointer text-red-400 hover:text-red-500 active:text-red-500"></i> 
-    <div>
-    `;
-    tasksContainer.appendChild(divContainer); //append the div container as the children of the mainPage
-    addTaskContainer.classList.add("hidden-class"); //hide addTaskContainer
-    inputTask.value = ""; //clear the input value
+createTaskBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (inputAddTask.value.trim() !== ""){
+    addTask(inputAddTask.value.trim());
+    inputAddTask.value = ""; //clear input value after adding
+    addTaskContainer.classList.add("hidden-class");
+  }
+});
+
+
+function addTask(taskTxt){
+  // let allTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  // allTasks.push(taskTxt);
+  // localStorage.setItem("tasks", JSON.stringify(allTasks));
+  // appendTaskToDOM(taskTxt);
+  let user = JSON.parse(localStorage.getItem("user"));
+  if (!user) return;
+  fetch(`http://localhost:3000/users/${user._id}/tasks`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      txt: taskTxt,
+      authToken: user.authToken
+    })
+  })
+  .then(res => res.json())
+  .then(data => {
+    if (data.error){
+      showError(data.error);
+    } else{
+      localStorage.setItem("tasks", JSON.stringify(data.tasks));
+      showTasks(data.tasks);
+    }
+  })
+  .catch(error => showError("Failed to add task!"));
+}
+
+function editTask(updatedText, idx){
+  let user = JSON.parse(localStorage.getItem("user"));
+  let allTasks = JSON.parse(localStorage).getItem("tasks") || [];
+  let task = allTasks.find(t => t.id === idx);
+
+  fetch(`http://localhost:3000/users/${user._id}/tasks/${task.id}`, {
+    method: "PATCH", 
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      txt: updatedText,
+      completed: task.completed,
+      authToken: user.authToken
+    })
+  })
+  .then(res => res.json())
+  .then(data => {
+    if(data.error){
+      showError(data.error);
+    } else{
+      localStorage.setItem("tasks", JSON.stringify(data.tasks));
+      showTasks(data.tasks);
+    }
+  })
+  .catch(error => showError("Failed to edit task!"));
+
+}
+
+function loadTasks(){
+  // const allTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  // // tasksContainer.innerHTML = ""; //clear previous tasks
+  // $(".mainTasks").innerHTML = ""
+  // allTasks.forEach((task, idx) => appendTaskToDOM(task, idx));
+  const user = JSON.parse(localStorage.getItem("user"));
+  if(!user){
+    return;
+  }
+
+  fetch(`http://localhost:3000/users/${user._id}/tasks`, {
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${user.authToken}`
+    }
+  })
+  .then(res => res.json())
+  .then(tasks => {
+    if(tasks.error){
+      showError(tasks.error);
+    }
+    else{
+      showTasks(tasks); 
+    }
+  })
+  .catch(error => showError("Failed to load tasks!"));
+
+}
+
+function showTasks(tasks){
+
+    const tasksContainer = document.querySelector(".mainTasks");
+    tasksContainer.innerHTML = "";
+
+    tasks.forEach(task =>{
+      let taskDivContainer = document.createElement("div");
+
+      taskDivContainer.setAttribute("data-index", task.id); //use data attribute for identification
+  
+      //add all necessary utility classes to divContainer for better UI
+      taskDivContainer.classList.add(
+        "flex",
+        "flex-row",
+        "rounded-sm",
+        "shadow-md",
+        "bg-slate-50",
+        "p-3",
+        "sm:p-4",
+        "my-3",
+        "mx-auto",
+        "justify-between",
+        "items-center",
+        "max-w-[35rem]"
+      );
+      taskDivContainer.innerHTML = `<p class="inputValPara">${taskTxt}<p>
+      <div class = "icons flex gap-4">
+      <i class="editIcon text-base sm:text-lg fa-solid fa-pen-to-square cursor-pointer hover:text-slate-600 active:text-slate-600"></i>
+      <i class="deleteIcon text-base sm:text-lg fa-solid fa-trash hover:cursor-pointer text-red-400 hover:text-red-500 active:text-red-500"></i> 
+      <div>
+      `;
+
+      // addTaskContainer.classList.add("hidden-class"); //hide addTaskContainer
+      // inputTask.value = ""; //clear the input value
+  
+      let deleteIcon = taskDivContainer.querySelector(".deleteIcon");
+      deleteIcon.addEventListener("click", ()=>{
+        // $(".mainTasks").removeChild(taskDivContainer);
+        const idx = this.getAttribute("data-id");
+        deleteTask(idx);
+        // loadTasks(); 
+      });
+
+      let editIcon = taskDivContainer.querySelector(".editIcon");
+    editIcon.addEventListener("click", ()=>{
+      const idx = this.getAttribute("data-id");
+      editTaskPrompt(task, idx);
+      
+    });
+    tasksContainer.appendChild(taskDivContainer); //append the taskdiv container as the children of the mainPage
+    })
+    
+
+    
+}
+
+function editTaskPrompt(task, id){
+  inputEditTask.value = task.txt;
+  currentEditingIdx = id;
+  editTaskContainer.classList.remove("hidden-class");
+      
+}
+
+let currentEditingIdx = null; //start edit index to null
+
+//editTask button action
+editTaskBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (inputEditTask.value.trim() !== "" && currentEditingIdx !== null){
+    editTask(inputEditTask.value.trim(), currentEditingIdx)
+    inputEditTask.value = ""; //clear input value after editing
+    editTaskContainer.classList.add("hidden-class");
+    // loadTasks();
+  }
+});
+
+// function updateTaskInLocalStorage(updatedTaskTxt, idx){
+//   let allTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+//   allTasks[idx] = updatedTaskTxt; //update the task at particular index
+//   localStorage.setItem("tasks", JSON.stringify(allTasks));
+// }
+
+
+function deleteTask(idx){
+  let user = JSON.parse(localStorage.getItem("user"));
+  let allTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  // allTasks.splice(idx, 1);
+  // localStorage.setItem("tasks", JSON.stringify(allTasks));
+
+  let task = allTasks[idx];
+
+  fetch(`http://localhost:3000/users/${user._id}/tasks/${task.id}`, {
+    method: "DELETE",
+    headers:{
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      authToken: user.authToken
+    })
+  })
+  .then(res => res.json())
+  .then(data => {
+    if(data.ok){
+      loadTasks();
+    } else{
+      showError(data.error || "Failed to delete task!");
+    }
+  })
+  .catch(error => showError("Failed to delete task!"));
+}
+
+//DOMContentLoaded
+document.addEventListener("DOMContentLoaded", () => {
+  if (
+    localStorage.getItem("isLogIn") === "true" ||
+    localStorage.getItem("isSignUp") === "true"
+  ) {
+    openMainPageScreen();
+  } else {
+    openLogInScreen();
   }
 });
